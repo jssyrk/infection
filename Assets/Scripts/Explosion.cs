274a1ff4609b46bfd63604f55a87ad424b3f6b71
Joss <JazAndJoss@Jazs-iMac.local>
2014-10-17 22:26:56 +0100

    Added advanced Zombie movement. Added death animation. New C Scripts added.

---
+++ Assets/Scripts/Explosion.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Explosion : MonoBehaviour {
+
+	public float destroyTime = 0.7f;
+
+	void Start(){
+		Invoke ("Die", destroyTime);
+	}
+
+	void Die(){
+		Destroy (gameObject);
+	}
+}
