4cebf0a820a3a91a9e75391ba1ed5eec4da325ff
BGB210 <room210@babbage>
2014-10-17 12:38:53 +0100

    Added new scripts to fix bullet damage and bullet destroy. Zombie now takes damage

---
+++ Assets/Scripts/ZombieBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieBehaviour : MonoBehaviour {
+
+	public int health = 10;
+
+	public void TakeDamage(int damage){
+		health -= damage;
+
+		if(health <= 0){
+			Destroy(gameObject);
+		}
+	}
+
+}
