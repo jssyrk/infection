39d1e63cb85f2086b045f132d6c8bd6778a6fcbe
BGB210 <room210@babbage>
2014-10-17 09:35:48 +0100

    Updated C scripts. Player movement, Camera Movement, and  Mouse Movement

---
+++ Assets/Scripts/MouseSmoothLook2D.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseSmoothLook2D : MonoBehaviour {
+
+	public Camera theCamera;
+	public float smoothing = 5.0f;
+	public float adjustmentAngle = 2.5f;
+
+	void Update(){
+		Vector3 target = theCamera.ScreenToViewportPoint (Input.mousePosition);
+
+		Vector3 difference = target - transform.position;
+		difference.Normalize ();
+
+		float rotZ = Mathf.Atan2 (difference.y, difference.x) * Mathf.Rad2Deg;
+		Quaternion newRotation = Quaternion.Euler (new Vector3 (0.0f, 0.0f, rotZ + adjustmentAngle));
+		transform.rotation = Quaternion.Lerp (transform.rotation, newRotation, Time.deltaTime * smoothing);
+	}
+
+}
--- Assets/Scripts/TopDownCharacterController2D.cs
+++ Assets/Scripts/TopDownCharacterController2D.cs
@@ -1,15 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
-
+	
+	[RequireComponent (typeof(Rigidbody2D))]
 public class TopDownCharacterController2D : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public float speed = 5.0f;
+
+	void FixedUpdate(){
+		float x = Input.GetAxis("Horizontal");
+		float y = Input.GetAxis("Vertical");
+
+		rigidbody2D.velocity = new Vector2 (x, y) * speed;
+		rigidbody2D.angularVelocity = 0.0f;
 	}
 }
