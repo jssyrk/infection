aa8ba942a87a1e548a8fec480c62c93fe458a843
BGB210 <room210@babbage>
2014-10-24 12:55:05 +0100

    Updated Zombie behaviors

--- Assets/Scripts/ZombieBehaviour.cs
+++ Assets/Scripts/ZombieBehaviour.cs
@@ -3,9 +3,33 @@ using System.Collections;
 
 public class ZombieBehaviour : MonoBehaviour {
 
+	private Transform player;
 	public int health = 10;
+	public int damage = 2;
 	public GameObject explosionPrefab;
 	public float adjustExplosionAngle = 0.0f;
+
+
+	void Start(){
+		if(GameObject.FindWithTag("Player")){
+			player = GameObject.FindWithTag ("Player").transform;
+
+			GetComponent<MoveTowardsObject>().target = player;
+			GetComponent<SmoothLookAtTarget2D>().target = player;
+		}
+	}
+
+	
+	void OnCollisionEnter2D(Collision2D other){
+		if(other.gameObject.CompareTag("Player")){
+				player = GameObject.FindWithTag ("Player").transform;
+
+				GetComponent<MoveTowardsObject>().target = player;
+				GetComponent<SmoothLookAtTarget2D>().target = player;
+		}
+	}
+
+
 
 	public void TakeDamage(int damage){
 		health -= damage;
@@ -14,9 +38,15 @@ public class ZombieBehaviour : MonoBehaviour {
 
 			Quaternion newRot = Quaternion.Euler(transform.eulerAngles.x, transform.eulerAngles.y, transform.eulerAngles.z + adjustExplosionAngle);
 			Instantiate(explosionPrefab, transform.position, newRot);
-
+					
+			GetComponent<AddScore>().DoSendScore();
 			Destroy(gameObject);
 		}
+	}
+
+	void FixedUpdate(){
+			rigidbody2D.velocity = Vector2.zero;
+			rigidbody2D.angularVelocity = 0.0f;
 	}
 
 }
