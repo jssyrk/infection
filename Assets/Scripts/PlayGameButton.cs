b891442b06fcd4c3dc3678b059e62bea9e54938d
Joss <JazAndJoss@Jazs-iMac.local>
2014-11-01 17:37:55 +0000

    Added Menu Scene. Added menu scene music.

---
+++ Assets/Scripts/PlayGameButton.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayGameButton : MonoBehaviour {
+
+	public string levelName = "Zombie Shooter Level 1";
+
+
+	void OnMouseUp(){
+		Application.LoadLevel(levelName);
+	}
+}
